6bf9e6af01ac2ad344029ff5029a3b452bb93842
David Welman <dwelman2704@gmail.com>
2017-10-16 15:10:45 +0200

    Tweaked rotations, added axis lines

---
+++ InTheShadows/Assets/Scripts/ContinuousRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuousRotation : MonoBehaviour
+{
+    public float rotationSpeed = 180;
+
+    private void Update()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0, Space.Self);
+    }
+}
---
+++ InTheShadows/Assets/Scripts/YAxisRotationFollower.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YAxisRotationFollower : MonoBehaviour
+{
+    public GameObject followObject;
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(followObject.transform.right);
+    }
+}
