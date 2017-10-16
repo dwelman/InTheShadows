1933d091bb534825127c2546f3e7c6cabeee6832
David WELMAN <daviwel@e4r25p1.wethinkcode.co.za>
2017-10-16 08:48:49 +0200

    Added horizontal and vertical controls

--- InTheShadows/Assets/Scripts/GameManager.cs
+++ InTheShadows/Assets/Scripts/GameManager.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+	public enum LevelControlTypes { Horizontal = 0, HorizontalAndVertical = 1, MultipleObjects = 2};
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public LevelControlTypes levelControlType;
+	public GameObject rotationObject;
+	public float rotationSpeed = 30f;
+
+	private Vector3 lastMousePos;
+
+	void Update()
+	{
+		Vector3 mousePos = Input.mousePosition;
+		Vector3 mouseDir = mousePos - lastMousePos;
+		if (levelControlType >= LevelControlTypes.HorizontalAndVertical && Input.GetKey(KeyCode.LeftControl))
+		{
+			if (Input.GetMouseButton(0))
+			{
+				rotationObject.transform.RotateAround(rotationObject.transform.position, new Vector3(1, 0, 0), rotationSpeed * mouseDir.y * Time.deltaTime);
+			}
+		}
+		else if (levelControlType >= LevelControlTypes.Horizontal)
+		{
+			if (Input.GetMouseButton(0))
+			{
+				rotationObject.transform.RotateAround(rotationObject.transform.position, new Vector3(0, 1, 0), rotationSpeed * mouseDir.x * Time.deltaTime * -1);
+			}
+		}
+		lastMousePos = mousePos;
 	}
 }
