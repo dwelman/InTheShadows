86602e02985886de83d309d93910e7e088bfefbb
Dean du Toit <dean100@live.co.za>
2017-10-17 17:08:57 +0200

    YANK!

---
+++ InTheShadows/Assets/Scripts/CardAnimation.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardAnimation : MonoBehaviour
+{
+    enum AnimState
+    {
+        STATIC,
+        YANK,
+        TOEND,
+        TOSTART,
+    }
+    public Vector3  endPos;
+    public Vector3  yankPos;
+    public float    moveSpeed;
+    public float    yankSpeed;
+    public Vector3  start;
+    bool            animating;
+    AnimState       animState;
+    public float    delay;
+    float           delayCount;
+
+	void Start ()
+    {
+        animState = AnimState.STATIC;
+        start = transform.position;
+    }
+
+    public void Yank()
+    {
+        animState = AnimState.YANK;
+        animating = true;
+    }
+
+    public void MoveToEnd()
+    {
+        animState = AnimState.TOEND;
+        animating = true;
+    }
+
+    void StepToStart()
+    {
+        float step = moveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, start, step);
+        if (transform.position == start)
+        {
+            animating = false;
+        }
+    }
+
+    void StepToEnd()
+    {
+       // delayCount += Time.deltaTime;
+        //if (delayCount > delay)
+        {
+            float step = moveSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, endPos, step);
+            if (transform.position == endPos)
+            {
+                animating = false;
+                delayCount = 0;
+            }
+        }
+    }
+
+    void StepToYank()
+    {
+        delayCount += Time.deltaTime;
+        if (delayCount > delay)
+        {
+            float step = yankSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, yankPos, step);
+            if (transform.position == yankPos)
+            {
+                animState = AnimState.TOEND;
+                delayCount = 0;
+            }
+        }
+    }
+
+    void Update ()
+    {
+		if (animating)
+        {
+            switch (animState)
+            {
+                case AnimState.STATIC:
+                    break;
+                case AnimState.TOEND:
+                    {
+                        StepToEnd();
+                        break;
+                    }
+                case AnimState.TOSTART:
+                    {
+                        StepToStart();
+                        break;
+                    }
+                case AnimState.YANK:
+                    {
+                        StepToYank();
+                        break;
+                    }
+            }
+        }
+	}
+}
---
+++ InTheShadows/Assets/Scripts/UIManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public Camera   camera;
+    bool            animating;
+    public CardAnimation   play, test, quit;
+
+   
+    enum State { MAIN, LEVELS, INGAME}
+    State state;
+	// Use this for initialization
+	void Start ()
+    {
+        state = State.MAIN;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log("You selected the " + hit.transform.tag);
+                if (hit.transform.CompareTag("PlayBtn"))
+                {
+                    state = State.LEVELS;
+                    play.Yank();
+                    test.Yank();
+                    quit.Yank();
+                }
+            }
+        }
+    }
+}
