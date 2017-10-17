e2019bd3ea2847454177b6e87793c3408079f745
David Welman <dwelman2704@gmail.com>
2017-10-17 14:40:14 +0200

    Added controls for each level

--- InTheShadows/Assets/Scripts/GameManager.cs
+++ InTheShadows/Assets/Scripts/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
 	Vector3 lastMousePos;
 	RotationObject currentRotationObject;
 	int currentObjectIndex = 0;
+    bool levelComplete = false;
 
 	void Awake()
 	{
@@ -26,61 +27,71 @@ public class GameManager : MonoBehaviour
 		Vector3 mousePos = Input.mousePosition;
 		Vector3 mouseDir = mousePos - lastMousePos;
 
-		currentRotationObject.axes.SetActive(true);
-		if (objectOriginPoint)
-		{
-			objectOriginPoint.axes.SetActive(false);
-		}
-		if (levelControlType >= LevelControlTypes.MultipleObjects && Input.GetKey(KeyCode.LeftShift))
-		{
-			currentRotationObject.axes.SetActive(false);
-			objectOriginPoint.axes.SetActive(true);
-			if (Input.GetMouseButton(0))
-			{
-				objectOriginPoint.transform.RotateAround(objectOriginPoint.transform.position, new Vector3(0, 1, 0), rotationSpeed * mouseDir.x * Time.deltaTime * -1);
-			}
-		}
-		else if (levelControlType >= LevelControlTypes.HorizontalAndVertical && Input.GetKey(KeyCode.LeftControl))
-		{
-			if (Input.GetMouseButton(0))
-			{
-                currentRotationObject.transform.Rotate(rotationSpeed * mouseDir.y * Time.deltaTime, 0, 0, Space.Self);
-			}
-		}
-		else if (levelControlType >= LevelControlTypes.Horizontal)
-		{
-			if (Input.GetMouseButton(0))
-			{
-				currentRotationObject.transform.RotateAround(currentRotationObject.transform.position, new Vector3(0, 1, 0), rotationSpeed * mouseDir.x * Time.deltaTime * -1);
-			}
-		}
+        if (!levelComplete)
+        {
+            currentRotationObject.axes.SetActive(true);
+            if (objectOriginPoint)
+            {
+                objectOriginPoint.axes.SetActive(false);
+            }
+            if (levelControlType >= LevelControlTypes.MultipleObjects && Input.GetKey(KeyCode.LeftShift))
+            {
+                currentRotationObject.axes.SetActive(false);
+                objectOriginPoint.axes.SetActive(true);
+                if (Input.GetMouseButton(0))
+                {
+                    objectOriginPoint.transform.RotateAround(objectOriginPoint.transform.position, new Vector3(0, 1, 0), rotationSpeed * mouseDir.x * Time.deltaTime * -1);
+                }
+            }
+            else if (levelControlType >= LevelControlTypes.HorizontalAndVertical && Input.GetKey(KeyCode.LeftControl))
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    currentRotationObject.transform.Rotate(rotationSpeed * mouseDir.y * Time.deltaTime, 0, 0, Space.Self);
+                }
+            }
+            else if (levelControlType >= LevelControlTypes.Horizontal)
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    currentRotationObject.transform.RotateAround(currentRotationObject.transform.position, new Vector3(0, 1, 0), rotationSpeed * mouseDir.x * Time.deltaTime * -1);
+                }
+            }
 
-		if (Input.GetKeyUp(KeyCode.R))
-		{
-			currentRotationObject.ResetRotation();
-		}
-		if (Input.GetKeyUp(KeyCode.Space))
-		{
-			SwitchCurrentRotationObject();
-		}
-
-		bool levelVictory = true;
-		if (objectOriginPoint)
-		{
-			levelVictory = objectOriginPoint.CheckVictoryCondition();
-		}
-		for (int i = 0; i < rotationObjects.Count; i++)
-		{
-			if (!rotationObjects[i].CheckVictoryCondition())
-			{
-				levelVictory = false;
-				break;
-			}
-		}
-		if (levelVictory)
-		{
-			Debug.Log("Level complete");
-		}
+            if (Input.GetKeyUp(KeyCode.R))
+            {
+                for (int i = 0; i < rotationObjects.Count; i++)
+                {
+                    rotationObjects[i].ResetRotation();
+                }
+                if (objectOriginPoint)
+                {
+                    objectOriginPoint.ResetRotation();
+                }
+            }
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                SwitchCurrentRotationObject();
+            }
+        }
+        bool levelVictory = true;
+        if (objectOriginPoint)
+        {
+            levelVictory = objectOriginPoint.CheckVictoryCondition();
+        }
+        for (int i = 0; i < rotationObjects.Count; i++)
+        {
+            if (!rotationObjects[i].CheckVictoryCondition())
+            {
+                levelVictory = false;
+                break;
+            }
+        }
+        if (levelVictory)
+        {
+            levelComplete = true;
+            Debug.Log("Level complete");
+        }
 		lastMousePos = mousePos;
 	}
 
