4a9a9f7459fb80af3be6bcded7f929c34b2592ed
David WELMAN <daviwel@e4r25p1.wethinkcode.co.za>
2017-10-20 08:40:04 +0200

    Added music player

---
+++ InTheShadows/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+	AudioSource source;
+
+	void Start ()
+	{
+		source = GetComponent<AudioSource>();
+		source.Play();
+	}
+
+	void Update()
+	{
+		if (PlayerPrefs.GetInt("sound_on") == 1)
+		{
+			source.volume = 1;
+		}
+		else
+		{
+			source.volume = 0;
+		}
+	}
+}
---
+++ InTheShadows/Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeManager : MonoBehaviour
+{
+	public GameObject volume;
+
+	bool state = true;
+
+	void Start()
+	{
+		if (PlayerPrefs.GetInt("sound_on") == 1)
+		{
+			state = true;
+			volume.SetActive(true);
+		}
+		else
+		{
+			state = false;
+			volume.SetActive(false);
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit))
+			{
+				if (hit.collider.tag == "volume")
+				{
+					Debug.Log("Here");
+					if (state == true)
+					{
+						PlayerPrefs.SetInt("sound_on", 0);
+						state = false;
+						volume.SetActive(false);
+					}
+					else
+					{
+						PlayerPrefs.SetInt("sound_on", 1);
+						state = true;
+						volume.SetActive(true);
+					}
+				}
+			}
+		}
+	}
+}
